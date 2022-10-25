089cf99f4dd90e2313dc3a3f35778db8aa8253d1
jordan <jordan@ceres.agency>
2022-10-25 10:12:12 +0200

    game loop update

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 # 1è façon avec True
 from random import randrange
+#from database.db import casino_db, generateId
 
-nb_coup = 0
-level = 0
+
+#users = casino_db.get_collection('users')
+#users.insert_one({'username': 'testRegister', 'password': 'testRegister', '_id': generateId(9)})
+#for user in users.find():
+#    print(user)
+
+authenticated = False
 
 #game loop  
 while True :
+    nb_coup = 0
+    level = 0
     # login or register
     while True:
         try:
-            login_or_register = int(input('Taper 1 pour vous connecter 0 pour vous enregistrer'))
+            login_or_register = int(input('Taper 1 pour vous connecter 0 pour vous enregistrer \n\n'))
             if(login_or_register == 0 or login_or_register == 1):
                 break
             else:
@@ -18,14 +26,16 @@ while True :
             print('Erreur')
     if(login_or_register == 1):
         levels = [str(1)]
+
     else:
         levels = [str(1), str(2)]
 
+    authenticated = True
     # choix du niveau
     while True:
         try:
             user_avaible_levels_string = ' | '.join(levels)
-            selected_level = int(input(f'Sélectionner un niveau parmis eux: ' + str(user_avaible_levels_string)))
+            selected_level = int(input(f'Sélectionner un niveau parmis eux: ' + str(user_avaible_levels_string) + '\n\n'))
             if(str(selected_level) in levels):
                 break
             else:
@@ -49,12 +59,12 @@ while True :
     while nb_coup != nb_legit_coup:
         while True:
             try:
-                nb_user = int(input ("Entrez SVP votre nombre ? "))
+                nb_user = int(input ("Entrez SVP votre nombre ? \n\n"))
                 break
             except:
                 print('Erreur')
         nb_coup += 1
-        if(nb_coup < nb_ordi):
+        if(nb_coup < nb_legit_coup):
             if nb_user > nb_ordi :
                 print ('Votre nbre est trop grand')
             elif nb_user < nb_ordi :
@@ -64,3 +74,13 @@ while True :
                 break
         else:
             print('vous avez perdu')
+    continue_casino = ''
+    while True:
+        try:
+            continue_casino = int(input("Voulez vous continuer ? 1 pour continuer 0 pour quitter \n\n"))
+            if(continue_casino == 0 or continue_casino == 1):
+                break
+        except:
+            print('Erreur vous devez choisir en entre 1 et 0 pour continuer ou quitter le jeu \n\n')
+    if(continue_casino == 0):
+        break
