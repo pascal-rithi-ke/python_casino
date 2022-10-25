ad74571aba5cba62f2e5b33e032a8a6cc3ae824d
Pascal Kien <pascal.rithi@gmail.com>
2022-10-25 11:20:51 +0200

    ajout champ username collection User et Partie

---
+++ FolderClass/Class.py
@@ -0,0 +1,19 @@
+import bcrypt
+
+class User:
+  def __init__(self, prenom, nom, id, username ,password, nbcoup):
+    self.prenom = prenom
+    self.username = username
+    self.nom = nom
+    self.id = id
+    self.password = password
+    self.nbcoup = nbcoup
+
+class Partie:
+  def __init__(self, idPartie, idUser, nomUser, prenomUser, username, User_nbcoup):
+    self.id = idPartie
+    self.idUser = idUser
+    self.nomUser = nomUser
+    self.prenomUser = prenomUser
+    self.username = username
+    self.User_nbcoup = User_nbcoup
